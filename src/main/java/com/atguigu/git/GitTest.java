b0b4a20731830218604040b1091ceddab0f8f72b
teamlin <2663712054@qq.com>
2022-09-01 11:21:33 +0800

    第2次更新

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("纵你阅人无数多 但无一人似我");
         System.out.println("失去你的我比国王富有,抱着你的我比乞丐落魄");
+        System.out.println("我没有懂得珍惜,如果上天给我再爱一次的机会");
     }
 }
